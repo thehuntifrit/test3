ff45db5c2051cec5a9d1459ca9749bdc1a4da707
thehuntifrit <the.hunt.ifrit@gmail.com>
2025-10-16 22:35:05 +0900

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,22 @@
+// firebase.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-firestore.js";
+import { getFunctions } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-functions.js";
+
+const FIREBASE_CONFIG = {
+  apiKey: "AIzaSyDAYv5Qm0bfqbHhCLeNp6zjKMty2y7xIIY",
+  authDomain: "the-hunt-49493.firebaseapp.com",
+  projectId: "the-hunt-49493",
+  storageBucket: "the-hunt-49493.firebasestorage.app",
+  messagingSenderId: "465769826017",
+  appId: "1:465769826017:web:74ad7e62f3ab139cb359a0",
+  measurementId: "G-J1KGFE15XP"
+};
+
+const app = initializeApp(FIREBASE_CONFIG);
+const db = getFirestore(app);
+const auth = getAuth(app);
+const functions = getFunctions(app, "asia-northeast2");
+
+export { app, db, auth, functions };
