016feb391df7db74075951a68f1ed3cddcf8e260
thehuntifrit <the.hunt.ifrit@gmail.com>
2025-10-17 02:50:39 +0900

    Update uiRender.js

--- src/uiRender.js
+++ src/uiRender.js
@@ -51,10 +51,11 @@ function createMobCard(mob) {
         </div>
       </div>
       <div class="progress-bar-wrapper h-4 rounded-full relative overflow-hidden transition-all duration-100 ease-linear">
-        <div class="progress-bar-bg absolute left-0 top-0 h-full rounded-full transition-all duration-100 ease-linear" style="width: 0;"></div>
-        <div class="progress-text absolute inset-0 flex items-center justify-center text-xs font-semibold" style="line-height: 1;">Calculating...</div>
+        <div class="progress-bar-bg absolute left-0 top-0 h-full rounded-full transition-all duration-100 ease-linear" style="width: ${mob.repopInfo?.elapsedPercent || 0}%"></div>
+        <div class="progress-text absolute inset-0 flex items-center justify-center text-xs font-semibold" style="line-height: 1;">${progressText}
       </div>
     </div>
+    
   `;
 
   const expandablePanelHTML = isExpandable ? `
