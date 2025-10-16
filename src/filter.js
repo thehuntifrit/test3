c917b245adc07a2ea5e0d3ed68040f3b66fdf7a8
thehuntifrit <the.hunt.ifrit@gmail.com>
2025-10-17 00:47:32 +0900

    Update filter.js

--- src/filter.js
+++ src/filter.js
@@ -1,7 +1,7 @@
 // filter.js
 import { getState } from "./store.js";
 import { EXPANSION_MAP } from "./store.js";
-import { DOM, FILTER_TO_DATA_RANK_MAP, EXPANSION_MAP } from "./uiShared.js";
+import { DOM, FILTER_TO_DATA_RANK_MAP } from "./uiShared.js";
 import { debounce } from "./utils.js";
 import { filterAndRender } from "./uiRender.js";
 
