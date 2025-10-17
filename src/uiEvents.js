c07f1e3e1c7e8888984d239a1c97187d947e0ddb
thehuntifrit <the.hunt.ifrit@gmail.com>
2025-10-17 13:28:27 +0900

    Update uiEvents.js

--- src/uiEvents.js
+++ src/uiEvents.js
@@ -2,7 +2,7 @@
 // uiEvents.js
 import { getState, setFilter, setOpenMobCardNo } from "./store.js";
 import { renderAreaFilterPanel, sortAndRedistribute } from "./filter.js";
-import { DOMElements, openReportModal, closeReportModal, submitReport, toJstAdjustedIsoString } from "./modal.js";
+import { DOMElements, openReportModal, closeReportModal, toJstAdjustedIsoString } from "./modal.js";
 import { DOM, FILTER_TO_DATA_RANK_MAP } from "./uiShared.js";
 import { submitReport, toggleCrushStatus } from "./firestore.js";
 import { debounce } from "./utils.js";
