18d14168d7c6f9f928211112e28cb713d44abfa5
thehuntifrit <the.hunt.ifrit@gmail.com>
2025-10-18 18:03:32 +0900

    Update uiRender.js

--- src/uiRender.js
+++ src/uiRender.js
@@ -53,7 +53,7 @@ const cardHeaderHTML = `
     <div class="flex-shrink-0 flex items-center justify-end">
       <button data-report-type="${rank === 'A' || rank === 'F' ? 'instant' : 'modal'}" data-mob-no="${mob.No}"
         class="w-8 h-8 flex items-center justify-center text-[12px] rounded bg-${rank === 'A' || rank === 'F' ? 'yellow' : 'green'}-500 
-        hover:bg-${rank === 'A' || rank === 'F' ? 'yellow' : 'green'}-400 text-gray-900 
+        hover:bg-${rank === 'A' || rank === 'F' ? 'yellow' : 'green'}-400 text-white 
         font-semibold transition text-center leading-tight whitespace-pre-line">${rank === 'A' || rank === 'F' ? '即時\n報告' : '報告\nする'}</button>
     </div>
   </div>
